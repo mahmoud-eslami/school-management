391ad6e85d2244d5b4494334e52698c6ab82101b
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-10 03:01:08 +0430

    add some part of userprofile model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
-# Create your models here.
+class userProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.user.username + ' # uuid : ' + str(self.uuid)
