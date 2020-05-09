eae103b141db2ee813885a2bce40fdd29920b361
MR.SS <sajjadrahimnouri@gmail.com>
2020-05-10 02:20:52 +0430

    databace added in sqllite

--- school/settings.py
+++ school/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+          'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         #'ENGINE': 'django.db.backends.mysql',
+        ##   'read_default_file': './my.cnf',
+
     }
 }
 
