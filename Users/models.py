d6e0122e999161891949d6955800177594cac6b6
MR.SS <sajjadrahimnouri@gmail.com>
2020-05-10 03:01:20 +0430

    add uuid in user profile model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import  User
+import uuid
+class UserProfile(models.Model):
 
-# Create your models here.
+    
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    uuid = models.UUIDField(primary_key = True, editable = False , default = uuid.uuid4)
+
+    def __str__(self):
+        return self.user.username +' uuid ==> '+ str(self.uuid)
