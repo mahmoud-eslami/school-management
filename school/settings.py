dc119cc84bbc4d206865a004904998d1d4d8ca78
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-10 02:19:17 +0430

    database added

--- school/settings.py
+++ school/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #    'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'OPTIONS': {
+            'read_default_file': './my.cnf',
+        },
     }
 }
 
