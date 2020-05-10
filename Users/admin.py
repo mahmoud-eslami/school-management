8db57d6fce148b95cfcc856d7ab7f1769844a095
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-10 12:43:58 +0430

    complete user profile model and add list_disply

--- Users/admin.py
+++ Users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import  *
 
-admin.site.register(userProfile)
+# this class make some field in django panel to show db information in better shape
+class customUserProfile(admin.ModelAdmin):
+    list_display = ['user','nationalCode','role','gender']
+
+# register user profile model to show in django panel
+admin.site.register(userProfile,customUserProfile)
--- Users/models.py
+++ Users/models.py
@@ -5,6 +5,36 @@ from django.contrib.auth.models import User
 class userProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    nationalCode = models.CharField(max_length=10,blank=False,null=False,default='xxxxxxxxxx')
 
+    #############################
+    #gender
+    MAN = '1'
+    WOMAN = '0'
+    #role
+    MODERATION = '1'
+    ASSISTANT = '2'
+    TEACHER = '3'
+    STUDENT = '4'
+    #############################
+    #############################
+    #gender choices
+    gender_choices = [
+        (MAN, 'آقا'),
+        (WOMAN, 'خانوم'),
+    ]
+    #role choices
+    role_choices = [
+        (MODERATION,'مدیر'),
+        (ASSISTANT,'معاون'),
+        (TEACHER,'معلم'),
+        (STUDENT,'دانش آموز'),
+    ]
+    #############################
+
+    gender = models.CharField(max_length=4,choices=gender_choices,default=MAN)
+    role = models.CharField(max_length=4,choices=role_choices,default=STUDENT)
+
+    #Magic Methods
     def __str__(self):
-        return self.user.username + ' # uuid : ' + str(self.uuid)
+        return self.user.username
