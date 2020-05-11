1376d5640474c4f05a9c582f3334763991ebee3b
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-11 18:45:33 +0430

    utf8 is fixed in mysql

--- school/settings.py
+++ school/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    #{
+    #    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    #},
 ]
 
 
