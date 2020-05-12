fc0b5cf238af2b8c373770f96b6b610c51a91a77
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-12 11:25:54 +0430

    fix all user method

--- Users/views.py
+++ Users/views.py
@@ -9,15 +9,17 @@ from .models import *
 class getAllUser(APIView):
 
     def get(self, request):
-        #try:
+        try:
             temp_users = User.objects.all()
-            temp_profile = userProfile.objects.all()
-            temp_doc = userDoc.objects.all()
+
             json_data = []
 
-            print(temp_profile)
+
 
             for item in temp_users:
+
+                temp_profile = userProfile.objects.get(user_id = item.id)
+                temp_doc = userDoc.objects.get(user_id = item.id)
 
                 json_data.append({
                     'id':item.id,
@@ -25,7 +27,27 @@ class getAllUser(APIView):
                     'name':item.first_name,
                     'lname':item.last_name,
                     'email':item.email,
+                    'userProfile':[
+                        {'uuid':temp_profile.uuid,
+                        'gender':temp_profile.gender,
+                        'role':temp_profile.role,}
+                    ],
+                    'userDoc':[
+                        {'address':temp_doc.address,
+                        'zipCode':temp_doc.zipCode,
+                        'citizen':temp_doc.citizen,
+                        'citizen_Num':temp_doc.citizen_Num,
+                        'personalCode':temp_doc.personalCode,
+                        'nationalCode':temp_doc.nationalCode,
+                        'nationalCardPhoto':str(temp_doc.nationalCardPhoto),
+                        'father_name':temp_doc.father_name,
+                        'father_nationalCode':temp_doc.father_nationalCode,
+                        'father_pNum':temp_doc.father_pNum,
+                        'mother_name':temp_doc.mother_name,
+                        'userPhoto':str(temp_doc.userPhoto),}
+                    ],
                 })
+
             return Response({"status_code":"200" ,"error":"","data": json_data ,"message":""},status.HTTP_200_OK)
-        #except:
-            #return Response({'error' : 'Internal Server Error'})
+        except:
+            return Response({'error' : 'Internal Server Error'})
