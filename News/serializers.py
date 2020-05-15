906513077ad2cff75fc2894f599c3cbec08eb524
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-15 16:04:06 +0430

    remove extera serilezer in news

--- News/serializers.py
+++ News/serializers.py
@@ -20,26 +20,3 @@ class NewsSerializer(serializers.Serializer):
         instance.pic = validated_data.get('title' , instance.pic)
         instance.save()
         return instance
-
-class allNewsSerializer(serializers.Serializer):
-    author_id = serializers.CharField(required = True,allow_null=False,allow_blank=False,max_length=10)
-    id = serializers.CharField(required = True,allow_null=False,allow_blank=False,max_length=10)
-    title = serializers.CharField(required=True,allow_null=False,allow_blank=False,max_length=200)
-    content = serializers.CharField(required=True,allow_null=False,allow_blank=False,max_length=2048)
-    release_date = serializers.DateTimeField(required=True,allow_null=False)
-    pic = serializers.ImageField(required=True,allow_null=True,allow_empty_file=True)
-
-class editNewsSerializer(serializers.Serializer):
-    id = serializers.CharField(required = True,allow_null=False,allow_blank=False,max_length=10)
-    title = serializers.CharField(required=True,allow_null=False,allow_blank=False,max_length=200)
-    content = serializers.CharField(required=True,allow_null=False,allow_blank=False,max_length=2048)
-    pic = serializers.ImageField(required=True,allow_null=True,allow_empty_file=True)
-
-class addNewsSerializer(serializers.Serializer):
-    author_id = serializers.CharField(required = True,allow_null=False,allow_blank=False,max_length=10)
-    title = serializers.CharField(required=True,allow_null=False,allow_blank=False,max_length=200)
-    content = serializers.CharField(required=True,allow_null=False,allow_blank=False,max_length=2048)
-    pic = serializers.ImageField(required=True,allow_null=True,allow_empty_file=True)
-
-class deleteNewsSerializer(serializers.Serializer):
-    news_id = serializers.CharField(required = True,allow_null=False,allow_blank=False,max_length=10)
