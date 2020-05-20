a430834ac7642ec0cf8f59f85d957a4115d12123
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-20 23:00:32 +0430

    add serializer for Classes

--- Classes/views.py
+++ Classes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from school.methods import *
+from school.validators import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import status
+import traceback
 
-# Create your views here.
+class GetAllClasses(APIView):
+    permission_classes=(IsAuthenticated,)
+    try:
+        
+    except Exception as e:
+
+class ClassesApi(APIView):
+    pass
