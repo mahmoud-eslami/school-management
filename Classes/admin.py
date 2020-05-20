770747d75db5890944697b763e19ece534abb0d4
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-20 20:50:00 +0430

    add classes model and register in admin page

--- Classes/admin.py
+++ Classes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Classes)
+class CustomClasses(admin.ModelAdmin):
+    list_display=['user_id','name','id']
--- Classes/models.py
+++ Classes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Classes(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40,blank=False,null=True)
