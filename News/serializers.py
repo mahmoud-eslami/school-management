6bc022e49e15b8bc311199da63c4a24aa91f498c
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-21 18:24:37 +0430

    some change in news serializer

--- News/serializers.py
+++ News/serializers.py
@@ -6,10 +6,10 @@ from school.validators import validate_image_size
 
 class NewsSerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True,allow_null=True,allow_blank=True,max_length=10)
-    title = serializers.CharField(max_length=20,allow_blank=False,allow_null=True)
-    content = serializers.CharField(max_length=200,allow_blank=False,allow_null=True)
-    author_id = serializers.CharField(allow_null=False,allow_blank=True,max_length=10)
-    pic = serializers.ImageField(max_length=None, allow_empty_file=False,use_url=True,validators=[validate_image_size,])
+    title = serializers.CharField(max_length=100,allow_blank=False,allow_null=False)
+    content = serializers.CharField(max_length=1024,allow_blank=False,allow_null=False)
+    author_id = serializers.CharField(max_length=10,allow_null=False,allow_blank=False)
+    pic = serializers.CharField(max_length=250,allow_null=False,allow_blank=False)
     release_date = serializers.DateTimeField(read_only=True)
 
     def create(self , validated_data):
