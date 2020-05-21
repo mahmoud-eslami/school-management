21998b39fea79e9b5d5f3076691f5cff34e72b57
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-22 01:05:07 +0430

    increase news content limit to 5k

--- News/models.py
+++ News/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class News(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=20,blank=False,null=True,unique=True)
-    content = models.CharField(max_length=1000,blank=False,null=True)
+    content = models.CharField(max_length=5000,blank=False,null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='author')
     pic = models.CharField(max_length=250,blank=True,null=True)
     release_date = models.CharField(max_length=50,blank=False,null=True)
