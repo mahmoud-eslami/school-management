b3db24a0b417697630412b31e86e149343692322
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-24 04:48:05 +0430

    add related field to news serializer

--- News/serializers.py
+++ News/serializers.py
@@ -3,14 +3,13 @@ from  django.contrib.auth.models import User
 from datetime import datetime
 from .models import *
 from school.validators import validate_image_size
+from Users.serializers import *
 
-class NewsSerializer(serializers.Serializer):
-    id = serializers.CharField(read_only=True,allow_null=True,allow_blank=True,max_length=10)
-    title = serializers.CharField(max_length=100,allow_blank=False,allow_null=False)
-    content = serializers.CharField(max_length=5000,allow_blank=False,allow_null=False)
-    author_id = serializers.CharField(max_length=10,allow_null=False,allow_blank=True)
-    pic = serializers.CharField(max_length=250,allow_null=False,allow_blank=False)
-    release_date = serializers.CharField(max_length=250,allow_null=False,allow_blank=False)
+class NewsSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+    class Meta:
+        model = News
+        fields = '__all__'
 
     def create(self , validated_data):
         return News.objects.create(**validated_data)
