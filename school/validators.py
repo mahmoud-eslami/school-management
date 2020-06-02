444e34c43a3082659089f9c80383d707ddba321f
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-06-02 16:07:16 +0430

    add password and reset code validator

--- school/validators.py
+++ school/validators.py
@@ -1,11 +1,24 @@
 from django.core.exceptions import ValidationError
+from Users.models import *
 import os
 
-def validate_reset_code(restCode):
-    pass
+def validate_reset_code(restCode,user_id):
+    user = MyUser.objects.get(id = user_id)
+    user_resetCode = user.reset_code
+    entered_resetCode_size = len(restCode)
+    if entered_resetCode_size != 7:
+        raise ValidationError("تعداد کاراکتر نامعتبر است.")
+    elif user_resetCode != restCode:
+        raise ValidationError("کد دو مرحله ای نامعتبر.")
+    else:
+        return True
 
 def validate_password_size(password):
-    pass
+    password_len = len(password)
+    if password_len < 8:
+        raise  ValidationError("پسورد حداقل 8 کارکتر است.")
+    else:
+        return password
 
 def validate_image_size(image):
     imagesize = image.size
