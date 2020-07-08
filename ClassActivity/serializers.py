8ea70098004f5909c8b80a82d615422cfa9228cf
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-07-08 10:16:44 +0430

    lesson deleted func added

--- ClassActivity/serializers.py
+++ ClassActivity/serializers.py
@@ -6,4 +6,7 @@ class LessonsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Lessons
         fields = '__all__'
+        read_only_fields = ['id']
 
+    def create(self , validated_data):
+        return Lessons.objects.create(**validated_data)
--- ClassActivity/urls.py
+++ ClassActivity/urls.py
@@ -3,4 +3,5 @@ from  . import views
 
 urlpatterns = [
     path('lessons/Api/',views.lessonsApi.as_view(),name='lesson_api'),
+    path('lessons/GetAllLesson/',views.GetAllLessons.as_view(),name='get_all_lesson'),
 ]
